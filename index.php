9a6ba8b171bf09c8b60f16abad7bb196459a8e0e
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2023-11-30 09:53:26 +0500

    js all code in index page script tag

--- index.php
+++ index.php
@@ -57,7 +57,44 @@ $res=mysqli_query($con,"select * from post");
       </div>
 
 
-    <script src="js/script.js"></script>
     <script src="js/jquery.js"></script>
+    <script>
+        function setLikeDislike(type,id){
+    jQuery.ajax({
+        url:'setLikeDislike.php',
+        type:'post',
+        data:'type='+type+'&id='+id,
+        success:function(result){
+            result=jQuery.parseJSON(result);
+            if(result.opertion=='like'){
+                jQuery('#like_'+id).removeClass('far');
+                jQuery('#like_'+id).addClass('fas');
+                jQuery('#dislike_'+id).addClass('far');
+                jQuery('#dislike_'+id).removeClass('fas');
+            }
+            if(result.opertion=='unlike'){
+                jQuery('#like_'+id).addClass('far');
+                jQuery('#like_'+id).removeClass('fas');
+            }
+            
+            if(result.opertion=='dislike'){
+                 jQuery('#dislike_'+id).removeClass('far');
+                 jQuery('#dislike_'+id).addClass('fas');
+                 jQuery('#like_'+id).addClass('far');
+                 jQuery('#like_'+id).removeClass('fas');
+            }
+            if(result.opertion=='undislike'){
+                jQuery('#dislike_'+id).addClass('far');
+                jQuery('#dislike_'+id).removeClass('fas');
+            }
+            
+            
+            jQuery('#post'+id+' #like').html(result.like_count);
+            jQuery('#post'+id+' #dislike').html(result.dislike_count);
+        }
+        
+    });
+}
+    </script>
 </body>
 </html>
