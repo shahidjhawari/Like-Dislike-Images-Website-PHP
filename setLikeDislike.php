2df951d859a8490ad63a38553b6ea914208ee978
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2023-11-30 09:45:11 +0500

    add cookie

--- setLikeDislike.php
+++ setLikeDislike.php
@@ -14,4 +14,10 @@
                     setcookie('dislike_'.$id,"yes",1);
                     mysqli_query($con,"update post set dislike_count=dislike_count-1 where id='$id'");
                 }
+                
+                setcookie('like_'.$id,"yes",time()+60*60*24*365*5);
+                $sql="update post set like_count=like_count+1 where id='$id'";
+                $opertion="like";
+            }
+        }
 ?>
