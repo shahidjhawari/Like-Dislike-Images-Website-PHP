f029fe2cef682ac20ba06987671cae1072058044
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2023-11-30 09:38:57 +0500

    jquery dom add

--- js/script.js
+++ js/script.js
@@ -3,7 +3,15 @@ function setLikeDislike(type,id){
         url:'setLikeDislike.php',
         type:'post',
         data:'type='+type+'&id='+id,
-        
+        success:function(result){
+            result=jQuery.parseJSON(result);
+            if(result.opertion=='like'){
+                jQuery('#like_'+id).removeClass('far');
+                jQuery('#like_'+id).addClass('fas');
+                jQuery('#dislike_'+id).addClass('far');
+                jQuery('#dislike_'+id).removeClass('fas');
+            }
+            
         
     });
 }
