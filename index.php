7aeaa2ae8e87be1d972f8abf8fff7fe9a08b3eaf
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2023-11-30 09:36:29 +0500

    images added

--- index.php
+++ index.php
@@ -29,23 +29,6 @@ $res=mysqli_query($con,"select * from post");
 				$dislikeClass="fas";
 			}	
 			?>
-            <article class="col-xs-12 col-sm-6 col-md-3" id="post<?php echo $row['id']?>">
-               <div class="panel panel-default">
-                  <div class="panel-body">
-                     <img src="images/<?php echo $row['image']?>" alt="<?php echo $row['title']?>" />
-                     </a>
-                  </div>
-                  <div class="panel-footer">
-                     <h4><?php echo $row['title']?></h4>
-                     <span class="pull-right">
-                        <i class="<?php echo $likeClass?> fa-thumbs-up" onclick="setLikeDislike('like','<?php echo $row['id']?>')" id="like_<?php echo $row['id']?>"></i>
-						<div id="like"><?php echo $row['like_count']?></div>
-						&nbsp;&nbsp;<i class="<?php echo $dislikeClass?> fa-thumbs-down" onclick="setLikeDislike('dislike','<?php echo $row['id']?>')" " id="dislike_<?php echo $row['id']?>"></i>
-						<div id="dislike"><?php echo $row['dislike_count']?></div>
-                     </span>
-                  </div>
-               </div>
-            </article>
 			<?php } } else {
 				echo "No data found";
 			}
