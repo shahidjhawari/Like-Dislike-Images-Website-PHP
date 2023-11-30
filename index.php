dbb7e715664ad1855cc2c1023bbde9ce0fb4119f
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2023-11-30 09:35:43 +0500

    images added & php code added

--- index.php
+++ index.php
@@ -13,6 +13,45 @@ $res=mysqli_query($con,"select * from post");
     <title>Document</title>
 </head>
 <body>
+<div class="container mt40">
+         <section class="row">
+			<?php 
+			if(mysqli_num_rows($res)>0){
+			while($row=mysqli_fetch_assoc($res)){
+				
+			$likeClass="far";	
+			if(isset($_COOKIE['like_'.$row['id']])){
+				$likeClass="fas";
+			}		
+
+			$dislikeClass="far";	
+			if(isset($_COOKIE['dislike_'.$row['id']])){
+				$dislikeClass="fas";
+			}	
+			?>
+            <article class="col-xs-12 col-sm-6 col-md-3" id="post<?php echo $row['id']?>">
+               <div class="panel panel-default">
+                  <div class="panel-body">
+                     <img src="images/<?php echo $row['image']?>" alt="<?php echo $row['title']?>" />
+                     </a>
+                  </div>
+                  <div class="panel-footer">
+                     <h4><?php echo $row['title']?></h4>
+                     <span class="pull-right">
+                        <i class="<?php echo $likeClass?> fa-thumbs-up" onclick="setLikeDislike('like','<?php echo $row['id']?>')" id="like_<?php echo $row['id']?>"></i>
+						<div id="like"><?php echo $row['like_count']?></div>
+						&nbsp;&nbsp;<i class="<?php echo $dislikeClass?> fa-thumbs-down" onclick="setLikeDislike('dislike','<?php echo $row['id']?>')" " id="dislike_<?php echo $row['id']?>"></i>
+						<div id="dislike"><?php echo $row['dislike_count']?></div>
+                     </span>
+                  </div>
+               </div>
+            </article>
+			<?php } } else {
+				echo "No data found";
+			}
+			?>
+		 </section>
+      </div>
 
 
     <script src="js/script.js"></script>
