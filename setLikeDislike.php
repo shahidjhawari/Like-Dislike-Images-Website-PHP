b2e42a5b4dd22f61d8603da1ae9934729d7333c3
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2023-11-30 09:44:15 +0500

    add php code

--- setLikeDislike.php
+++ setLikeDislike.php
@@ -1,3 +1,17 @@
-<?php require("database.php") 
-
+<?php require("database.php"); 
+    if(isset($_POST['type']) && $_POST['type']!='' && isset($_POST['id']) && $_POST['id']>0){
+        $type=mysqli_real_escape_string($con,$_POST['type']);
+        $id=mysqli_real_escape_string($con,$_POST['id']);
+        
+        if($type=='like'){
+            if(isset($_COOKIE['like_'.$id])){
+                setcookie('like_'.$id,"yes",1);
+                $sql="update post set like_count=like_count-1 where id='$id'";
+                $opertion="unlike";
+            }else{
+                
+                if(isset($_COOKIE['dislike_'.$id])){
+                    setcookie('dislike_'.$id,"yes",1);
+                    mysqli_query($con,"update post set dislike_count=dislike_count-1 where id='$id'");
+                }
 ?>
